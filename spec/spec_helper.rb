8eb006d502d7d1e5e01e18bfaefdd501860ae737
Taynara Jacon <tjacon@partnerfusion.com>
2019-06-24 13:37:50 -0300

    Update screenshot conf

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,7 @@ RSpec.configure do |config|
 
   config.after(:example) do |e|
     name = e.description.gsub(/[^A-Za-z0-9 ]/, '').tr(' ', '_')
-    page.save_screenshot('log/' + name + '.png')
+    page.save_screenshot('log/' + name + '.png') if e.exception
   end
 end
 
